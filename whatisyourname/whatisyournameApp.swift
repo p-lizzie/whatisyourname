7dedd398a84cdcb7e2d9414bcbfb99e734543acf
Paige <paige.e.leung@gmail.com>
2023-06-26 10:31:30 -0700

    Initial Commit

---
+++ whatisyourname/whatisyournameApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatisyournameApp.swift
+//  whatisyourname
+//
+//  Created by Paige Leung on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct whatisyournameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
